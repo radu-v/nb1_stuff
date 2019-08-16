131a968ca1bc3c0bd4df5936ff30e4d12f304931
Radu Vasilascu <radu.vasilascu@kneat.com>
2019-08-16 14:16:04 +0100

    Revert last two commits and add some power config from stock

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
-aapt package -M AndroidManifest.xml -S res -F framework-res.overlay.autobrightness.apk.u -I %android-28-sdk%\android.jar -o android.overlay.autobrightness
-"C:\Program Files\Java\jdk1.8.0_181\bin\jarsigner.exe" -keystore .keystore framework-res.overlay.autobrightness.apk.u overlay
+aapt package -M AndroidManifest.xml -S res -F framework-res.overlay.autobrightness.apk.u -I %android-sdk%\android.jar -o android.overlay.autobrightness
+apksigner sign --ks keystore.jks framework-res.overlay.autobrightness.apk.u
 zipalign 4 framework-res.overlay.autobrightness.apk.u framework-res.overlay.autobrightness.apk
