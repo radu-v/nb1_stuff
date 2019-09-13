7cd95657a4a9d8a2d8155497e6ab02d8247d40ff
Radu Vasilascu <radu.vasilascu@kneat.com>
2019-09-13 10:41:54 +0100

    Added android.jar to make life easier

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
-aapt package -M AndroidManifest.xml -S res -F framework-res.overlay.autobrightness.apk.u -I %android-sdk%\android.jar -o android.overlay.autobrightness
+aapt package -M AndroidManifest.xml -S res -F framework-res.overlay.autobrightness.apk.u -I platforms\android-28\android.jar -o android.overlay.autobrightness
 apksigner sign --ks keystore.jks framework-res.overlay.autobrightness.apk.u
 zipalign 4 framework-res.overlay.autobrightness.apk.u framework-res.overlay.autobrightness.apk
