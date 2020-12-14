5d8fb73414d5b9032713d42f62cd68cc353ef70a
Radu Vasilascu <glx33807@gmail.com>
2020-12-14 12:04:19 +0000

    Add deviceLightCapabilities and update platform to 30

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-aapt package -M AndroidManifest.xml -S res -F framework-res.overlay.autobrightness.apk.u -I platforms/android-28/android.jar -o android.overlay.autobrightness
+aapt package -M AndroidManifest.xml -S res -F framework-res.overlay.autobrightness.apk.u -I platforms/android-30/android.jar -o android.overlay.autobrightness
 
 apksigner sign --ks keystore.jks framework-res.overlay.autobrightness.apk.u
 
