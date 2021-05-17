2e9f1aa70bfa296c6f2a00cbc527a8c52bb53e04
Radu Vasilascu <glx33807@gmail.com>
2021-05-17 12:23:28 +0100

    Add gsi fixes module

---
+++ nb1_gsi-fixes_magisk-module/README.md
@@ -0,0 +1,10 @@
+# Introduction
+
+This module applies some fixes for Nokia 8:
+- audio volume curves for making volume tracker more consistent with output
+- adds a handful of missing SELinux policies
+
+## What you need
+
+* Magisk v21.1+
+* Custom ROM for Nokia 8
---
+++ nb1_gsi-fixes_magisk-module/post-fs-data.sh
@@ -0,0 +1,5 @@
+chown system:system /sys/devices/soc/soc:qcom,cpubw/devfreq/soc:qcom,cpubw/min_freq
+chown system:system /sys/class/kgsl/kgsl-3d0/devfreq/min_freq
+chown system:system /sys/class/kgsl/kgsl-3d0/devfreq/max_freq
+chown system:system /sys/class/devfreq/soc:qcom,gpubw/min_freq
+chown system:system /dev/stune/top-app/schedtune.boost
